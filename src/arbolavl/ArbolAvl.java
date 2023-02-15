e1d52c1d3230611b7bac248114977b6a3122b580
Ana Patricia Hernandez Lara <122854097+NugetDePatto@users.noreply.github.com>
2023-02-14 20:34:45 -0600

    comentario

--- src/arbolavl/ArbolAvl.java
+++ src/arbolavl/ArbolAvl.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ArbolAvl {
+
     Nodo raiz;
 
     void insertarBin(int num) {
@@ -24,52 +25,52 @@ public class ArbolAvl {
         }
         return aux;
     }
+
     void imprimir() {
         Queue<Nodo> cola = new LinkedList<Nodo>();
 
         if (raiz != null) {
             cola.add(raiz);
 
-                while (!cola.isEmpty()) {
-                    if (cola.element().izq != null) {
-                        cola.add(cola.element().izq);
-                    }
-
-                    if (cola.element().der != null) {
-                        cola.add(cola.element().der);
-                    }
-
-                    System.out.print("["+cola.element().num + " " + cola.poll().altura + "]");
-
+            while (!cola.isEmpty()) {
+                if (cola.element().izq != null) {
+                    cola.add(cola.element().izq);
                 }
+
+                if (cola.element().der != null) {
+                    cola.add(cola.element().der);
+                }
+
+                System.out.print("[" + cola.element().num + " " + cola.poll().altura + "]");
+
+            }
         }
 
         System.out.println("");
 
     }
 
-    
-    void altura(){
+    void altura() {
         altura(raiz);
     }
-  
-    void altura(Nodo nodo){
-        if(nodo != null){
+
+    void altura(Nodo nodo) {
+        //profe... no se ria de mi codigo
+        if (nodo != null) {
             altura(nodo.izq);
             altura(nodo.der);
-            if(nodo.der == null && nodo.izq == null){ //si es hoja
+            if (nodo.der == null && nodo.izq == null) { //si es hoja
                 nodo.altura = 1;
-            } else if (nodo.izq == null){ //si no es hoja y no tiene hijo izq
+            } else if (nodo.izq == null) { //si no es hoja y no tiene hijo izq
                 nodo.altura = nodo.der.altura + 1;
-            } else if (nodo.der == null){//si no tiene hijo der
+            } else if (nodo.der == null) {//si no tiene hijo der
                 nodo.altura = nodo.izq.altura + 1;
-            } else if(nodo.izq.altura > nodo.der.altura){ // si no es hoja, compara quien es el mayor de lso hijos
+            } else if (nodo.izq.altura > nodo.der.altura) { // si no es hoja, compara quien es el mayor de lso hijos
                 nodo.altura = nodo.izq.altura + 1;
             } else {
                 nodo.altura = nodo.der.altura + 1;
             }
         }
     }
-    
-  
+
 }
