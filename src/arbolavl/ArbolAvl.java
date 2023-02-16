3b781efc3ebe4de866090af735ada6cb7491712e
Ana Patricia Hernandez Lara <122854097+NugetDePatto@users.noreply.github.com>
2023-02-15 23:52:07 -0600

    Me costo, que confusa es la recursividad JJAAJ

--- src/arbolavl/ArbolAvl.java
+++ src/arbolavl/ArbolAvl.java
@@ -70,46 +70,19 @@ public class ArbolAvl {
     */
     
     void altura(){
-        altura(raiz);
+        System.out.println(altura(raiz));
     }
     
-    void altura (Nodo nodo){
+    int altura (Nodo nodo){
         if(nodo != null){
-            int izq = 0, der = 0;
-            altura(nodo.izq);
-            altura(nodo.der);
-            if(nodo.izq != null) izq = nodo.izq.altura;
-            if(nodo.der != null) der = nodo.der.altura;
-            nodo.altura = Integer.max(izq, der) + 1;
+            int izq = altura(nodo.izq);
+            int der = altura(nodo.der);
+            
+            int mayor = Integer.max(izq, der);
+            nodo.altura = mayor + 1;
+            return nodo.altura;
+        } else {
+            return 0;
         }
     }
 }
-
-/*
-if (nodo.der == null && nodo.izq == null) { //si es hoja
-                nodo.altura = 1;
-            } else if (nodo.izq == null) { //si no es hoja y no tiene hijo izq
-                nodo.altura = nodo.der.altura + 1;
-            } else if (nodo.der == null) {//si no tiene hijo der
-                nodo.altura = nodo.izq.altura + 1;
-            } else if (nodo.izq.altura > nodo.der.altura) { // si no es hoja, compara quien es el mayor de lso hijos
-                nodo.altura = nodo.izq.altura + 1;
-            } else {
-                nodo.altura = nodo.der.altura + 1;
-            }
-
-
-int alto = 0;
-            if (nodo.izq == null) {//puede ser una hoja
-                alto = 1;
-                if (nodo.der != null)//si la derecha no es null entonces no es hoja y solo tiene dercha
-                    alto = nodo.der.altura + 1;
-            } else {//no es hoja pero puede no tener un hijo
-                alto = nodo.izq.altura + 1;
-                if (nodo.der != null) //si tiene los dos hijos
-                    alto = Integer.max(nodo.izq.altura, nodo.der.altura) + 1;
-            }
-            nodo.altura = alto;
-
-
- */
