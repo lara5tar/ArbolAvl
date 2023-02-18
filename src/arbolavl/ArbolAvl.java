d0a0b24d1395ede9b99e50e0436d71ae8f5a53c9
Ana Patricia Hernandez Lara <122854097+NugetDePatto@users.noreply.github.com>
2023-02-17 20:24:43 -0600

    mod

--- src/arbolavl/ArbolAvl.java
+++ src/arbolavl/ArbolAvl.java
@@ -8,31 +8,25 @@ public class ArbolAvl {
     Nodo raiz;
 
     void insertarBin(int num) {
-        if (raiz == null) {
-            raiz = new Nodo(num);
-        } else {
-            raiz = insertarBin(raiz, num);
-        }
+        if (raiz == null) raiz = new Nodo(num);
+        else raiz = insertarBin(raiz, num);
     }
 
     Nodo insertarBin(Nodo aux, int num) {
-        if (aux == null) {
-            return new Nodo(num);
-        } else if (num < aux.num) {
-            aux.izq = insertarBin(aux.izq, num);
-        } else {
-            aux.der = insertarBin(aux.der, num);
-        }
+        if (aux == null) return new Nodo(num);
+        
+        else if (num < aux.num) aux.izq = insertarBin(aux.izq, num);
+        
+        else aux.der = insertarBin(aux.der, num);
         
         int izq = altura(aux.izq);
         int der = altura(aux.der);
         
-        if(izq - der < -1){ //carga a la derecha -2 o menos
-            aux = rotacionIzq(aux, altura(aux.der.izq), altura(aux.der.der));
-        }
-        if(izq - der > 1){ // carga a la izquierda 2 o mas
-            aux = rotacionDer(aux, altura(aux.izq.izq), altura(aux.izq.der));
-        }
+        //carga a la derecha -2 o menos
+        if(izq - der < -1) aux = rotacionIzq(aux, altura(aux.der.izq), altura(aux.der.der));
+        
+        // carga a la izquierda 2 o mas
+        if(izq - der > 1) aux = rotacionDer(aux, altura(aux.izq.izq), altura(aux.izq.der));
         
         return aux;
     }
@@ -66,16 +60,11 @@ public class ArbolAvl {
             cola.add(raiz);
 
             while (!cola.isEmpty()) {
-                if (cola.element().izq != null) {
-                    cola.add(cola.element().izq);
-                }
+                if (cola.element().izq != null) cola.add(cola.element().izq);
 
-                if (cola.element().der != null) {
-                    cola.add(cola.element().der);
-                }
-
+                if (cola.element().der != null) cola.add(cola.element().der);
+                
                 System.out.print("[" + cola.element().num + " " + cola.poll().altura + "]");
-
             }
         }
 
@@ -91,11 +80,9 @@ public class ArbolAvl {
         if(nodo != null){
             int izq = altura(nodo.izq);
             int der = altura(nodo.der);
-            int mayor = Integer.max(izq, der);
-            nodo.altura = mayor + 1;
-            return nodo.altura; 
-        } else {
-            return 0;
-        }
+            int mayor = Integer.max(izq, der) + 1;
+            return nodo.altura = mayor; 
+        } else  return 0;
+        
     }
 }
